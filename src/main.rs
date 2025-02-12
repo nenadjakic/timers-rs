4bb75ad37290e9d41be872d1d6968fdfc83b5fc6
nenadjakic <nenad.jakic@gmail.com>
2025-02-12 18:02:03 +0100

    Externalize configuration to config.toml file

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
 use std::io::Result;
 
+use settings::Settings;
+
 pub mod app;
 pub mod crossterm;
 pub mod ui;
 pub mod model;
 pub mod repository;
 pub mod error;
+pub mod settings;
 
 fn main() -> Result<()> {
     env_logger::init();
-    let _ = crossterm::run();
+    match Settings::new() {
+        Ok(settings) => {
+            let _ = crossterm::run(settings);
+        },
+        Err(e) => eprintln!("Failed to load settings: {}", e),
+    }    
 
     Ok(())
 }
